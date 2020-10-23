303ab5d9007bca2e775d181774069a5b3cd7aaf5
4m3li3 <50198726+4m3li3@users.noreply.github.com>
2020-10-23 22:01:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-const demo = document.getElementById("demo");
-
-const title = demo.innerHTML;
-
-const charArray = title.split('')
-
-let animationHTML = '';
-
-charArray.forEach((char, idex) => {
-  animationHTML = animationHTML + `<div class='char' style='animation-delay:${idex*2}s' >${char}</div>`
-})
-
-demo.innerHTML = animationHTML;
+// Animations init
+new WOW().init();
